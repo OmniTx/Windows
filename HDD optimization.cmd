30f83f9645a1e8a62d8305e6fa318e41594ab56e
OmniTx <60448236+OmniTx@users.noreply.github.com>
2021-06-09 21:56:00 +0600

    Update HDD optimization.cmd

--- HDD optimization.cmd
+++ HDD optimization.cmd
@@ -1,10 +1,9 @@
 @echo off
-color 2
+color 3
 fstuil set memory query usage
 fstuil set memory query usage 2
 fsutil behavior set disableLastAccess 1
 fsutil behavior set disable8dot3 1 
-
 cls
 echo ram  and hdd is optimized!
 pause
